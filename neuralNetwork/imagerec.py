247eb4038f22a126b32d364b0804c5350c4bef3e
dbanasiak <damian.banasiak96@gmail.com>
2018-12-19 23:54:16 +0100

    image recognition - numbers

---
+++ neuralNetwork/imagerec.py
@@ -0,0 +1,81 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import functools
+from collections import Counter
+
+def createExamples():
+    numberArrayExamples = open('numArEx.txt', 'a')
+    numbersWeHave = range(1, 10)
+    versionWeHave = range(1, 10)
+
+    for eachNum in numbersWeHave:
+        for eachVer in versionWeHave:
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(eachVer)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei);
+            eiar1 = str(eiar.tolist())
+
+            lineToWrite = str(eachNum)+'::'+eiar1+'\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+def threshold(imageArray):
+    balanceAr = []
+    newAr = imageArray
+
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = functools.reduce(lambda x, y: x + y, eachPix[:3])/len(eachPix[:3])
+            balanceAr.append(avgNum)
+
+    balance = functools.reduce(lambda x, y: x + y, balanceAr)/len(balanceAr)
+
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if  functools.reduce(lambda x, y: x + y, eachPix[:3])/len(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+def whatNumIsThis(filePath):
+    matchedAr = []
+    loadExamps = open('numArEx.txt', 'r').read()
+    loadExamps = loadExamps.split('\n')
+
+    i = Image.open(filePath)
+    iar = np.array(i)
+    iarl = iar.tolist()
+
+    inQuestion = str(iarl)
+
+    for eachExample in loadExamps:
+        if len(eachExample) > 3:
+            splitEx = eachExample.split('::')
+            currentNum = splitEx[0]
+            currentAr = splitEx[1]
+
+            #zmien to w przyszlosci
+            eachPixEx = currentAr.split('],')
+            eachPixInQ = inQuestion.split('],')
+
+            x = 0
+
+            while x < len(eachPixEx):
+                if eachPixEx[x] == eachPixInQ[x]:
+                    matchedAr.append(int(currentNum))
+
+                x += 1
+
+    x = Counter(matchedAr)
+
+    print(x)
+
+whatNumIsThis('images/test.png')
