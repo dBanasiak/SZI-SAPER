a7d98f547acb11eb929708c4f05437239c74634b
dbanaiask <damian.banasiak96@gmail.com>
2019-01-10 22:12:41 +0100

    refactor v2 - use lists - delete randBomb file

--- mapGeneratorPygame/setBomb.py
+++ mapGeneratorPygame/setBomb.py
@@ -2,21 +2,36 @@ from mapGeneratorPygame.emptyMatrix import emptyMatrix
 import random
 
 mergedMatrix = emptyMatrix()
-bombPos = []
+bombProp = []
+
+# Ścieżki dla 6 przykładowych bomb
+bomb1Path = '../neuralNetwork/images/test1.png'
+bomb2Path = '../neuralNetwork/images/test2.png'
+bomb3Path = '../neuralNetwork/images/test3.png'
+bomb4Path = '../neuralNetwork/images/test4.png'
+bomb5Path = '../neuralNetwork/images/test5.png'
+bomb6Path = '../neuralNetwork/images/test6.png'
+
+# Lista ścieżek do bomb
+bombPathArray = ['empty', 'saper', bomb1Path, bomb2Path, bomb3Path, bomb4Path, bomb5Path, bomb6Path]
 
 def randBombPos():
     i = 0
-    while i < 9:
+    while i <= 9:
         x = random.randrange(0, 9)
         y = random.randrange(0, 9)
-
         bomb = random.randrange(2, 7)
 
         if(mergedMatrix[x][y] == 0):
             mergedMatrix[x][y] = bomb
             i += 1
-            bombPos.append((x, y))
+            bombProp.append((x, y, bomb, bombPathArray[bomb], x * 64, y * 64, i))
 
-def returnBombAndPos():
+def printBombAndPos():
     randBombPos()
-    return mergedMatrix, bombPos
+    print('\nBomb properties:\n', bombProp)
+    print('\nSet bomb:')
+    for row in mergedMatrix:
+        print(row)
+    return bombProp
+
