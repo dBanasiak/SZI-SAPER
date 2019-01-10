e00cd01f1a0b6d42bd313e6f4018d3945c2cd7d8
dbanaiask <damian.banasiak96@gmail.com>
2019-01-10 11:20:47 +0100

    Rand bomb

---
+++ mapGeneratorPygame/setBomb.py
@@ -0,0 +1,24 @@
+from mapGeneratorPygame.emptyMatrix import emptyMatrix
+import random
+
+mergedMatrix = emptyMatrix()
+
+def randBombPos():
+    i = 0
+    while i < 9:
+        x = random.randrange(0, 9)
+        y = random.randrange(0, 9)
+
+        bomb = random.randrange(2, 7)
+
+        if(mergedMatrix[x][y] == 0):
+            mergedMatrix[x][y] = bomb
+            i += 1
+            print('Position - X: ', x, ' Y: ', y, ' Bomb: ', bomb)
+        else:
+            print('Field is not empty')
+
+def setBomb():
+    print('setBomb')
+
+randBombPos()
