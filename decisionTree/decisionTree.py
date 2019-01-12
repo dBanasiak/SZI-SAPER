bf37908a144939a58f0b509072d5369e4d59963a
Wiktor <wiktorbalaban@gmail.com>
2019-01-12 11:27:17 +0100

    Code cleaning

--- decisionTree/decisionTree.py
+++ decisionTree/decisionTree.py
@@ -2,43 +2,43 @@
 We'll write a Decision Tree Classifier, in pure Python.
 """
 
-
 # For Python 2 / 3 compatability
 from __future__ import print_function
 
 import sys;
+
 training_data = [
-    ['zegarowa' , 10, 100000, 1],
-    ['zegarowa' , 30, 100000, 2],
-    ['zegarowa' , 60, 100000, 2],
-    ['zegarowa' , 10, 10000, 2],
-    ['zegarowa' , 30, 10000, 4],
-    ['zegarowa' , 60, 10000, 4],
-    ['zegarowa' , 10, 1000, 2],
-    ['zegarowa' , 30, 1000, 4],
-    ['zegarowa' , 60, 1000, 4],
-    ['dynamit' , 5, 100000, 3],
-    ['dynamit' , 10, 10000, 3],
-    ['dynamit' , 15, 1000, 5],
-    ['dynamit' , 5, 10000, 3],
-    ['dynamit' , 10, 1000, 3],
-    ['dynamit' , 15,1000, 5],
-    ['dynamit' , 5, 100000, 3],
-    ['dynamit' , 10, 10000, 3],
-    ['dynamit' , 15, 1000, 5],
-    ['cieplna' , sys.maxsize, 100000, 6],
-    ['cieplna' , sys.maxsize, 10000, 6],
-    ['cieplna' , sys.maxsize, 1000, 6],
-    ['biologiczna' , sys.maxsize, 10000000, 8],
-    ['biologiczna' , sys.maxsize, 1000000, 8],
-    ['biologiczna' , sys.maxsize, 100000, 8],
-    ['przeciwpiechotna' , sys.maxsize, 100000, 9],
-    ['przeciwpiechotna' , sys.maxsize, 10000, 9],
-    ['przeciwpiechotna' , sys.maxsize, 1000, 9],
-    ['przeciwpancerna' , sys.maxsize, 10000000, 10],
-    ['przeciwpancerna' , sys.maxsize, 1000000, 10],
-    ['przeciwpancerna' , sys.maxsize, 100000, 10],
-    
+    ['zegarowa', 10, 100000, 1],
+    ['zegarowa', 30, 100000, 2],
+    ['zegarowa', 60, 100000, 2],
+    ['zegarowa', 10, 10000, 2],
+    ['zegarowa', 30, 10000, 4],
+    ['zegarowa', 60, 10000, 4],
+    ['zegarowa', 10, 1000, 2],
+    ['zegarowa', 30, 1000, 4],
+    ['zegarowa', 60, 1000, 4],
+    ['dynamit', 5, 100000, 3],
+    ['dynamit', 10, 10000, 3],
+    ['dynamit', 15, 1000, 5],
+    ['dynamit', 5, 10000, 3],
+    ['dynamit', 10, 1000, 3],
+    ['dynamit', 15, 1000, 5],
+    ['dynamit', 5, 100000, 3],
+    ['dynamit', 10, 10000, 3],
+    ['dynamit', 15, 1000, 5],
+    ['cieplna', sys.maxsize, 100000, 6],
+    ['cieplna', sys.maxsize, 10000, 6],
+    ['cieplna', sys.maxsize, 1000, 6],
+    ['biologiczna', sys.maxsize, 10000000, 8],
+    ['biologiczna', sys.maxsize, 1000000, 8],
+    ['biologiczna', sys.maxsize, 100000, 8],
+    ['przeciwpiechotna', sys.maxsize, 100000, 9],
+    ['przeciwpiechotna', sys.maxsize, 10000, 9],
+    ['przeciwpiechotna', sys.maxsize, 1000, 9],
+    ['przeciwpancerna', sys.maxsize, 10000000, 10],
+    ['przeciwpancerna', sys.maxsize, 1000000, 10],
+    ['przeciwpancerna', sys.maxsize, 100000, 10],
+
 ]
 
 # Column labels.
@@ -49,6 +49,7 @@ header = ["type", "time to off", "cost", "priorytet"]
 def unique_vals(rows, col):
     """Find the unique values for a column in a dataset."""
     return set([row[col] for row in rows])
+
 
 def class_counts(rows):
     """Counts the number of each type of example in a dataset."""
@@ -61,9 +62,11 @@ def class_counts(rows):
         counts[label] += 1
     return counts
 
+
 def is_numeric(value):
     """Test if a value is numeric."""
     return isinstance(value, int) or isinstance(value, float)
+
 
 class Question:
     """A Question is used to partition a dataset.
@@ -95,6 +98,7 @@ class Question:
         return "Is %s %s %s?" % (
             header[self.column], condition, str(self.value))
 
+
 def partition(rows, question):
     """Partitions a dataset.
     For each row in the dataset, check if it matches the question. If
@@ -108,6 +112,7 @@ def partition(rows, question):
             false_rows.append(row)
     return true_rows, false_rows
 
+
 def gini(rows):
     """Calculate the Gini Impurity for a list of rows.
     There are a few different ways to do this, I thought this one was
@@ -118,8 +123,9 @@ def gini(rows):
     impurity = 1
     for lbl in counts:
         prob_of_lbl = counts[lbl] / float(len(rows))
-        impurity -= prob_of_lbl**2
+        impurity -= prob_of_lbl ** 2
     return impurity
+
 
 def info_gain(left, right, current_uncertainty):
     """Information Gain.
@@ -128,6 +134,7 @@ def info_gain(left, right, current_uncertainty):
     """
     p = float(len(left)) / (len(left) + len(right))
     return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
 
 def find_best_split(rows):
     """Find the best question to ask by iterating over every feature / value
@@ -163,6 +170,7 @@ def find_best_split(rows):
                 best_gain, best_question = gain, question
 
     return best_gain, best_question
+
 
 class Leaf:
     """A Leaf node classifies data.
@@ -228,18 +236,18 @@ def print_tree(node, spacing=""):
 
     # Base case: we've reached a leaf
     if isinstance(node, Leaf):
-        print (spacing + "Predict", node.predictions)
+        print(spacing + "Predict", node.predictions)
         return
 
     # Print the question at this node
-    print (spacing + str(node.question))
+    print(spacing + str(node.question))
 
     # Call this function recursively on the true branch
-    print (spacing + '--> True:')
+    print(spacing + '--> True:')
     print_tree(node.true_branch, spacing + "  ")
 
     # Call this function recursively on the false branch
-    print (spacing + '--> False:')
+    print(spacing + '--> False:')
     print_tree(node.false_branch, spacing + "  ")
 
 
@@ -258,6 +266,7 @@ def classify(row, node):
     else:
         return classify(row, node.false_branch)
 
+
 def print_leaf(counts):
     """A nicer way to print the predictions at a leaf."""
     total = sum(counts.values()) * 1.0
@@ -266,50 +275,51 @@ def print_leaf(counts):
         probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
     return probs
 
+
 if __name__ == '__main__':
 
     my_tree = build_tree(training_data)
 
     print_tree(my_tree)
-    print(classify(['zegarowa' , 9, 1000000], my_tree))
+    print(classify(['zegarowa', 9, 1000000], my_tree))
 
     # Evaluate
     testing_data = [
-# typ bomby, ile min do wybuchu, jakie straty spowoduje w zł, priorytet
-        ['zegarowa' , 10, 100000, 1],
-        ['zegarowa' , 30, 100000, 2],
-        ['zegarowa' , 60, 100000, 2],
-        ['zegarowa' , 10, 10000, 2],
-        ['zegarowa' , 30, 10000, 4],
-        ['zegarowa' , 60, 10000, 4],
-        ['zegarowa' , 10, 1000, 2],
-        ['zegarowa' , 30, 1000, 4],
-        ['zegarowa' , 60, 1000, 4],
-        ['dynamit' , 5, 100000, 3],
-        ['dynamit' , 10, 10000, 3],
-        ['dynamit' , 15, 1000, 5],
-        ['dynamit' , 5, 10000, 3],
-        ['dynamit' , 10, 1000, 3],
-        ['dynamit' , 15,1000, 5],
-        ['dynamit' , 5, 100000, 3],
-        ['dynamit' , 10, 10000, 3],
-        ['dynamit' , 15, 1000, 5],
-        ['cieplna' , sys.maxsize, 100000, 6],
-        ['cieplna' , sys.maxsize, 10000, 6],
-        ['cieplna' , sys.maxsize, 1000, 6],
-        ['biologiczna' , sys.maxsize, 10000000, 8],
-        ['biologiczna' , sys.maxsize, 1000000, 8],
-        ['biologiczna' , sys.maxsize, 100000, 8],
-        ['przeciwpiechotna' , sys.maxsize, 100000, 9],
-        ['przeciwpiechotna' , sys.maxsize, 10000, 9],
-        ['przeciwpiechotna' , sys.maxsize, 1000, 9],
-        ['przeciwpancerna' , sys.maxsize, 10000000, 10],
-        ['przeciwpancerna' , sys.maxsize, 1000000, 10],
-        ['przeciwpancerna' , sys.maxsize, 100000, 10],
-        ['bomba z sody oczyszczonej' , 1, 10, 11],
-        
+        # typ bomby, ile min do wybuchu, jakie straty spowoduje w zł, priorytet
+        ['zegarowa', 10, 100000, 1],
+        ['zegarowa', 30, 100000, 2],
+        ['zegarowa', 60, 100000, 2],
+        ['zegarowa', 10, 10000, 2],
+        ['zegarowa', 30, 10000, 4],
+        ['zegarowa', 60, 10000, 4],
+        ['zegarowa', 10, 1000, 2],
+        ['zegarowa', 30, 1000, 4],
+        ['zegarowa', 60, 1000, 4],
+        ['dynamit', 5, 100000, 3],
+        ['dynamit', 10, 10000, 3],
+        ['dynamit', 15, 1000, 5],
+        ['dynamit', 5, 10000, 3],
+        ['dynamit', 10, 1000, 3],
+        ['dynamit', 15, 1000, 5],
+        ['dynamit', 5, 100000, 3],
+        ['dynamit', 10, 10000, 3],
+        ['dynamit', 15, 1000, 5],
+        ['cieplna', sys.maxsize, 100000, 6],
+        ['cieplna', sys.maxsize, 10000, 6],
+        ['cieplna', sys.maxsize, 1000, 6],
+        ['biologiczna', sys.maxsize, 10000000, 8],
+        ['biologiczna', sys.maxsize, 1000000, 8],
+        ['biologiczna', sys.maxsize, 100000, 8],
+        ['przeciwpiechotna', sys.maxsize, 100000, 9],
+        ['przeciwpiechotna', sys.maxsize, 10000, 9],
+        ['przeciwpiechotna', sys.maxsize, 1000, 9],
+        ['przeciwpancerna', sys.maxsize, 10000000, 10],
+        ['przeciwpancerna', sys.maxsize, 1000000, 10],
+        ['przeciwpancerna', sys.maxsize, 100000, 10],
+        ['bomba z sody oczyszczonej', 1, 10, 11],
+
     ]
 
     for row in testing_data:
-        print ("Actual priority: %s. Predicted: %s" %
-               (row[-1], print_leaf(classify(row, my_tree))))
+        print("Actual priority: %s. Predicted: %s" %
+              (row[-1], print_leaf(classify(row, my_tree))))
